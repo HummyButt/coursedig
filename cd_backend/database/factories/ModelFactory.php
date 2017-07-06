ac6fab7a5ba0a519493673c72b7acf6a08d01512
Ameer Harram <ameerharram@gmail.com>
2017-07-06 22:46:11 +0500

    Project Push too github
    Project is pushing to github for Development

---
+++ cd_backend/database/factories/ModelFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'firstName' => $faker->name,
+        'lastName' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('abc123'),
+        'remember_token' => str_random(10),
+    ];
+});
